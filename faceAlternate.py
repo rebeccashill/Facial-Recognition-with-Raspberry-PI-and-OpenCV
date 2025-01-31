74985b9a350e1501f71d67d1be91706c6fa6d7f7
Rebecca Shillingford <51681289+rebeccashill@users.noreply.github.com>
2025-01-31 16:08:11 -0500

    Update faceAlternate.py
    Add comments for each of the lines of code.

--- faceAlternate.py
+++ faceAlternate.py
@@ -1,10 +1,16 @@
 import cv2 
 
+#Load cascade file for detecvting faces
 eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.1.0/data/haarcascades/ha$ 
 face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.1.0/data/haarcascades/h$ 
 
+#Load an image from file
 img = cv2.imread('sachin.jpg') 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+
+#Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Look for faces in the image using the loaded cascade file
 faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
 print "Found "+str(len(faces))+" face(s)" 
 
